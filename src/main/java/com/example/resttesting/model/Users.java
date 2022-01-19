9d6888bd2639c6b84b937ca16d2511a03927f68d
olowo-coder <olowofela.olanrewaju@decagon.dev>
2022-01-19 13:20:58 +0100

    test

---
+++ src/main/java/com/example/resttesting/model/Users.java
@@ -0,0 +1,11 @@
+package com.example.resttesting.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Users {
+    private Long id;
+    private String name;
+}
---
+++ src/test/java/com/example/resttesting/RestTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resttesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
